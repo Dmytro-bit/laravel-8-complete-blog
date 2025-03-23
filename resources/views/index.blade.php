9735ab59441aaacd5676e4088e551d8c34d36da6
dmytro <dziumanenkod@gmail.com>
2025-03-23 23:56:40 +0000

    3 recent blogs main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,42 +18,23 @@
         </div>
     </div>
     <div class="text-center py-15">
-            <span class="uppercase text-s text-gray-400">
-                Blog
-            </span>
+        <span class="uppercase text-s text-gray-400">
+            Blog
+        </span>
 
         <h2 class="text-4xl font-bold py-10">
             Recent Posts
         </h2>
 
-        <p class="m-auto w-4/5 text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos
-            temporibus quaerat facere consectetur qui.
-        </p>
-    </div>
-
-    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
-        <div class="flex bg-yellow-700 text-gray-100 pt-10">
-            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
-                    <span class="uppercase text-xs">
-                        PHP
-                    </span>
-
-                <h3 class="text-xl font-bold py-10">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error
-                    culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur
-                    facilis quasi ex? Nemo hic placeat et?
-                </h3>
-
-                <a
-                    href=""
-                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-                    Find Out More
-                </a>
-            </div>
-        </div>
-        <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+            @foreach ($recentPosts as $post)
+                <div class="bg-white p-6 rounded-lg shadow-lg">
+                    <img src="{{ asset('images/' . $post->image_path) }}" alt="{{ $post->title }}" class="w-full h-32 object-cover mb-4 rounded">
+                    <h3 class="text-2xl font-bold mb-2">{{ $post->title }}</h3>
+                    <p class="text-gray-700 mb-4">{{ Str::limit($post->description, 100) }}</p>
+                    <a href="/blog/{{ $post->id }}" class="text-blue-500">Read More</a>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
