9557ace2aa5ae6f2262aaa04a566f186615cba85
dmytro <dziumanenkod@gmail.com>
2025-03-23 20:43:51 +0000

    update show blog post

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            {{ $post->title }}
-        </h1>
+    <div class="w-4/5 m-auto text-center">
+        <div class="py-15">
+            <h1 class="text-6xl font-bold leading-tight">
+                {{ $post->title }}
+            </h1>
+        </div>
     </div>
-</div>
 
-<div class="w-4/5 m-auto pt-20">
-    <span class="text-gray-500">
-        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
+    <div class="w-4/5 m-auto pt-20 text-left">
+        <span class="text-gray-500">
+            By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+        </span>
 
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->description }}
-    </p>
-</div>
-
-@endsection 
+        <div class="flex py-10">
+            <div class="w-1/3">
+                <img src="{{ asset('images/' . $post->image_path) }}" alt="{{ $post->title }}" class="w-full h-auto rounded-lg">
+            </div>
+            <div class="w-2/3 pl-10">
+                <p class="text-xl text-gray-700 leading-8 font-light">
+                    {{ $post->description }}
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
