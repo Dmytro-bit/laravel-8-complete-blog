70b1d28ca954c3845f34be4bf2b3fce83fb7ef8f
dmytro <dziumanenkod@gmail.com>
2025-03-23 23:57:29 +0000

    3 recent blogs main

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $recentPosts = Post::orderBy('created_at', 'desc')->take(3)->get();
+        return view('index', compact('recentPosts'));
     }
 
     public function about_us()
